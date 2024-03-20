32803c629ae76a8b08eb174af0f716b7b464a25e
Maider Landaribar <maiderlandaribar@Maiders-MacBook-Pro.local>
2024-03-20 12:41:51 +0100

    test: check if get to / returns todos list

--- app.test.js
+++ app.test.js
@@ -3,7 +3,21 @@ const app = require("././app");
 //const server = require("./server");
 
 describe("GET /", () => {
-  it("GET / should return object", () => {
-    return request(app).get("/").expect(200);
+  it("GET / should return status 200", () => {
+    return request(app).get("/").expect(200).expect("Content-Type", /json/);
+  });
+  it("GET / should return array of objects with 'name' and 'description' as keys.", () => {
+    return request(app)
+      .get("/")
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              name: expect.any(String),
+              description: expect.any(String),
+            }),
+          ])
+        );
+      });
   });
 });
--- routes/todos.js
+++ routes/todos.js
@@ -2,8 +2,19 @@ const express = require("express");
 const router = express.Router();
 const createError = require("http-errors");
 
+let todos = [
+  {
+    name: "dni",
+    description: "llamar por teléfono para coger cita de renovación",
+  },
+  {
+    name: "pintar",
+    description: "Comprar pintura y pintar sala y comedor",
+  },
+];
+
 router.get("/", (req, res) => {
-  res.status(200).json();
+  res.status(200).json(todos);
 });
 
 module.exports = router;
